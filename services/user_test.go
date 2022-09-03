2645ec489d50065b973de5f9bd52e0ce43beee41
Conor Kelly <conormpkelly@gmail.com>
2022-09-03 22:11:56 +0100

    tests that ensure users service returns errors from DB

---
+++ services/user_test.go
@@ -0,0 +1,144 @@
+package services
+
+import (
+	"fmt"
+	"log"
+	"testing"
+
+	"github.com/conormkelly/fiber-demo/database"
+	"github.com/conormkelly/fiber-demo/models"
+	"github.com/stretchr/testify/assert"
+)
+
+type databaseTest struct {
+	description string                   // Description of the test case
+	action      func(*UserService) error // A func that relays the error from the service method
+}
+
+// Confirm that a specific error is returned by the service method when DB is unavailable
+func TestDatabaseDown(t *testing.T) {
+	testCases := []databaseTest{
+		{
+			description: "CreateUser with DB offline",
+			action: func(svc *UserService) error {
+				_, err := svc.CreateUser("Joe", "Bloggs")
+				return err
+			},
+		},
+		{
+			description: "GetAllUsers with DB offline",
+			action: func(svc *UserService) error {
+				_, err := svc.GetAllUsers()
+				return err
+			},
+		},
+		{
+			description: "GetUser with DB offline",
+			action: func(svc *UserService) error {
+				_, err := svc.GetUser(1)
+				return err
+			},
+		},
+		{
+			description: "UpdateUser with DB offline",
+			action: func(svc *UserService) error {
+				firstName := "Joe"
+				lastName := "Bloggs"
+				_, err := svc.UpdateUser(1, &firstName, &lastName)
+				return err
+			},
+		},
+		{
+			description: "DeleteUser with DB offline",
+			action: func(svc *UserService) error {
+				return svc.DeleteUser(1)
+			},
+		},
+	}
+
+	// Test setup / arrangement
+	db := database.Database{}
+	modelsToMigrate := []interface{}{&models.User{}}
+	err := db.Connect(&database.Options{UseInMemoryDatabase: true, ModelsToMigrate: modelsToMigrate})
+	if err != nil {
+		log.Fatal("Database failed to connect: " + err.Error())
+	}
+
+	// Close the database so that the calls to it in the service fail
+	databaseConnection, err := db.Conn.DB()
+	if err != nil {
+		log.Fatal("Failed to get DB connection: " + err.Error())
+	}
+	databaseConnection.Close()
+
+	userService := &UserService{DB: &db}
+
+	// Act
+	expectedErrorMessage := "sql: database is closed"
+	executeDbTests(userService, t, testCases, &expectedErrorMessage)
+}
+
+// Confirm that a specific error is returned by the service methods when table doesn't exist
+func TestNonExistentTable(t *testing.T) {
+	testCases := []databaseTest{
+		{
+			description: "CreateUser with no users table",
+			action: func(svc *UserService) error {
+				_, err := svc.CreateUser("Joe", "Bloggs")
+				return err
+			},
+		},
+		{
+			description: "GetAllUsers with no users table",
+			action: func(svc *UserService) error {
+				_, err := svc.GetAllUsers()
+				return err
+			},
+		},
+		{
+			description: "GetUser with no users table",
+			action: func(svc *UserService) error {
+				_, err := svc.GetUser(1)
+				return err
+			},
+		},
+		{
+			description: "UpdateUser with no users table",
+			action: func(svc *UserService) error {
+				firstName := "Joe"
+				lastName := "Bloggs"
+				_, err := svc.UpdateUser(1, &firstName, &lastName)
+				return err
+			},
+		},
+		{
+			description: "DeleteUser with no users table",
+			action: func(svc *UserService) error {
+				return svc.DeleteUser(1)
+			},
+		},
+	}
+
+	// Test setup / arrangement
+	// Creating a DB without any tables
+	db := database.Database{}
+	err := db.Connect(&database.Options{UseInMemoryDatabase: true})
+	if err != nil {
+		log.Fatal("Database failed to connect: " + err.Error())
+	}
+
+	userService := &UserService{DB: &db}
+
+	expectedErrorMessage := "no such table: users"
+	executeDbTests(userService, t, testCases, &expectedErrorMessage)
+}
+
+func executeDbTests(svc *UserService, t *testing.T, testCases []databaseTest, expectedErrorMessage *string) {
+	for _, test := range testCases {
+		t.Run(fmt.Sprintf("%s - %s", t.Name(), test.description), func(t *testing.T) {
+			err := test.action(svc)
+			assert.NotNil(t, err, fmt.Sprintf("%s did not return the expected error.", test.description))
+			assert.Equal(t, *expectedErrorMessage, err.Error())
+		})
+	}
+}
