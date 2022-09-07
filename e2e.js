57c213b8c522b9cd5dacd0c91cea2e90db30ef02
Conor Kelly <conormpkelly@gmail.com>
2022-09-07 18:47:23 +0100

    refactor to use functions in e2e test orchestration script

--- e2e.js
+++ e2e.js
@@ -1,43 +1,44 @@
 const newman = require('newman')
 const { execSync, spawn } = require('child_process')
 
+// Config
 const SCRIPT_TIMEOUT_MS = 30000
 
 async function main() {
   console.time('Time taken')
 
   // Create a subprocess of the docker-compose process
-  console.log('RUNNING: docker-compose-up')
+  console.log('RUNNING: docker-compose up')
   const dockerProcess = spawn('docker-compose', ['up'])
+  dockerProcess.stdout.setEncoding('utf8')
 
-  // Set a timer in case app doesn't start successfully
+  // Set a timer in case the app doesn't start successfully
   const startupTimer = setTimeout(() => {
     console.error(
       `\nFAILED: app startup took greater than ${SCRIPT_TIMEOUT_MS} ms`
     )
-    try {
-      execSync('docker-compose down')
-    } catch (err) {
-      console.log('ERROR: Failed to run docker-compose down')
-      dockerProcess.kill()
-    }
+    shutdownDockerProcess(dockerProcess)
   }, SCRIPT_TIMEOUT_MS)
 
-  // Wait for Fiber to start
-  dockerProcess.stdout.on('data', async (chunk) => {
-    const output = chunk.toString('utf-8')
-    console.log(output)
-    if (output.includes('bound on')) {
+  // Wait for Fiber to start within docker subprocess
+  // by monitoring the stdout
+  dockerProcess.stdout.on('data', async (data) => {
+    // const output = data.toString('utf-8')
+    console.log(data)
+    if (data.includes('bound on')) {
+      // App has started up, so clear the timer
       clearTimeout(startupTimer)
-      console.log('APP: READY')
-      await executePostmanCollection('./Fiber_Gorm.postman_collection.json')
+
+      let exitCode = 0
       try {
-        execSync('docker-compose down')
+        await executePostmanCollection('./Fiber_Gorm.postman_collection.json')        
       } catch (err) {
-        console.log('ERROR: Failed to run docker-compose down')
-        dockerProcess.kill()        
+        exitCode = 1
+        console.error('An error occured executing postman collection:', err.message)
+      } finally {
+        shutdownDockerProcess(dockerProcess)
       }
-      process.exit(0)
+      process.exit(exitCode)
     }
   })
 
@@ -52,6 +53,16 @@ async function main() {
     clearTimeout(startupTimer)
     process.exit(1)
   })
+}
+
+function shutdownDockerProcess(process) {
+  try {
+    console.log('RUNNING: docker-compose down')
+    execSync('docker-compose down')
+  } catch (err) {
+    console.log('ERROR: Failed to run docker-compose down')
+    process.kill()
+  }
 }
 
 function executePostmanCollection(path) {
